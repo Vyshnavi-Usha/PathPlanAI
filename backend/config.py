0312e2efa70b8bf1349f20ccd71f58ee8b708645
Vyshnavi P <vyp@paloaltonetworks.com>
2025-07-29 23:34:23 +0530

    first commit

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,3 @@
-PROJECT_ID = "itd-ai-interns" 
-REGION = "us-central1"
-MODEL_NAME = "gemini-2.5-flash"
-# MODEL_NAME = "gemini-2.5-flash-preview-05-20"
+PROJECT_ID = "" 
+REGION = ""
+MODEL_NAME = ""
